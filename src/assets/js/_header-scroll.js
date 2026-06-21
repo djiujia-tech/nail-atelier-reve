d2fca2b0c7bf270d03910cc9486a64030f3c6b7e
djiujia-tech <djiujia@gmail.com>
2026-06-22 01:36:26 +0900

    fix: no-inner-declarations eslint error

--- src/assets/js/_header-scroll.js
+++ src/assets/js/_header-scroll.js
@@ -3,13 +3,13 @@ const header = document.querySelector(".p-header");
 if (header) {
   const THRESHOLD = 80;
 
-  function updateHeader() {
+  const updateHeader = () => {
     if (window.scrollY > THRESHOLD) {
       header.classList.add("is-scrolled");
     } else {
       header.classList.remove("is-scrolled");
     }
-  }
+  };
 
   window.addEventListener("scroll", updateHeader, { passive: true });
   updateHeader();
